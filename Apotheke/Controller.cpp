512818b53c0798d1416856f7b1e86234384c9fa6
rares04 <raresastilean@yahoo.com>
2020-04-05 13:06:24 +0300

    Controller Klasse

---
+++ Apotheke/Controller.cpp
@@ -0,0 +1,49 @@
+#include "Controller.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+Controller::Controller(Repository _repo) {
+	this->repo = _repo;
+}
+
+
+void Controller::printRepo() const {
+	for (std::size_t i = 0; i < repo.getSize(); ++i)
+		cout << repo.getMedikament_byIndex(i).toString();
+	cout << endl << endl;
+}
+
+
+void Controller::knapp(double x) const {
+	cout << "\nDie Medikamente, die die Menge kleiner als " << x << " haben, sind:\n";
+	for (std::size_t i = 0; i < repo.getSize(); ++i)
+		if (repo.getMedikament_byIndex(i).getMenge() < x)
+			cout << repo.getMedikament_byIndex(i).toString();
+	cout << endl << endl;
+}
+
+
+void Controller::strFind(std::string substr) {
+	if (substr == "") {
+		cout << "\nSubstring war leer, eine Liste mit alle Medikamente wird angezeigt:\n";
+		repo.sortByName();
+		this->printRepo();
+	}
+	else {
+		cout << "\nEs werden die Medikamente angezeigt, die '" << substr << "' enthalten:\n";
+		for (std::size_t i = 0; i < repo.getSize(); ++i)
+			if (repo.getMedikament_byIndex(i).getName().find(substr) != string::npos)
+				cout << repo.getMedikament_byIndex(i).toString();
+		cout << endl << endl;
+	}
+}
+
+
+void Controller::groupPrice() {
+	cout << "\nEs werden die Medikamente, nach Preis gruppiert, angezeigt:\n";
+	repo.sortByPrice();
+	this->printRepo();
+}
---
+++ Apotheke/Controller.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Repository.h"
+#include <string>
+
+
+class Controller {
+private:
+	Repository repo;
+public:
+	// Constructor
+	Controller(Repository repo);
+
+	// Prints the list with medications
+	void printRepo() const;
+
+	// Shows only the medications, which have the amount smaller than x
+	void knapp(double x) const;
+
+	// Shows only the medications, which contain the substring "substr", sorted after name of the medication
+	void strFind(std::string substr);
+
+	// Shows the list with medications grouped by price
+	void groupPrice();
+
+};
